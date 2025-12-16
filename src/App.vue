d5c441ed785f421ad2068319a726c249d75629fe
Jey223 <julieudoh7@gmail.com>
2025-12-16 09:23:21 -0500

    changed Projects, due to issues

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <the-header></the-header>
+</template>
+
+<script>
+import TheHeader from './components/layouts/TheHeader.vue'
+
+export default {
+  components: {
+    TheHeader,
+  },
+}
+</script>
+
+<style scoped></style>
