2064a356a3a0cf1b20328723f59c3362af2aad8c
Jey223 <julieudoh7@gmail.com>
2025-12-18 08:03:27 -0500

    nothing changed

--- src/components/UI/BaseCard.vue
+++ src/components/UI/BaseCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mx-auto max-w-2xl my-12 rounded-xl shadow-[0_2px_8px_rgba(0,0,0,0.26)] p-5">
+  <div class="mx-auto max-w-2xl my-10 rounded-xl shadow-[0_2px_8px_rgba(0,0,0,0.26)] p-5">
     <slot></slot>
   </div>
 </template>
